95de3bc770def477539edc437614aa55d51ebadb
John Turner <john.turner@engineering.digital.dwp.gov.uk>
2022-11-23 13:56:10 +0000

    amend initialize_k8s to not automatically deploy green

--- initialize_k8s.sh
+++ initialize_k8s.sh
@@ -5,4 +5,5 @@ set -e
 kubectl apply -f starter/apps/hello-world
 kubectl apply -f starter/apps/canary/index_v1_html.yml
 kubectl apply -f starter/apps/canary/canary-v1.yml
-kubectl apply -f starter/apps/blue-green
+kubectl apply -f starter/apps/blue-green/blue.yml
+kubectl apply -f starter/apps/blue-green/index_blue_html.yml
